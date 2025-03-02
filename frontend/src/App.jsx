037d3ff4465c5ea3ad3b010a41571aeabfc0ed0e
Dev-Alase <devalase5@gmail.com>
2025-03-02 23:20:23 +0530

    Added a login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
